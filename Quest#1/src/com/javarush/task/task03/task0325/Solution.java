257f1126e84ab3fc1a961d3673a5452784b1aac2
androsevgen <andros.evgen@gmail.com>
2017-01-18 00:02:28 +0200

    commit

---
+++ Quest#1/src/com/javarush/task/task03/task0325/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task03.task0325;
+
+
+/* 
+Финансовые ожидания
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+    }
+}
